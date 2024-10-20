0abcfadd30cbd5c527ccdee16714975ee6dcdaf6
Durga mohan <93484022+DurgaM821@users.noreply.github.com>
2024-10-20 21:13:58 +0530

    Create StaticMethodMain.java
    Optional main class, how to call a static methods

---
+++ StaticMethodMain.java
@@ -0,0 +1,14 @@
+package ConstructorHandsOn;
+
+public class StaticMethodMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StaticMethodsEx st1=new StaticMethodsEx();
+		StaticMethodsEx st2=new StaticMethodsEx();
+
+		System.out.println(st1.add(21, 22));
+	}
+
+}
