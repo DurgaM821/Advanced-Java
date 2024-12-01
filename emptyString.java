10fa3b19ae8fa2528e82e8f61c76bcf15be7c6df
Durga mohan <93484022+DurgaM821@users.noreply.github.com>
2024-12-01 16:56:59 +0530

    Add files via upload
    The isEmpty() method is commonly used in scenarios where you need to check if a string contains any meaningful content before performing further operations on it.

---
+++ emptyString.java
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		 String message = "Java is fun!";
+        System.out.println(message.charAt(11));
+        
+         
+        String text = "Hello, Java!";
+        String emptyString = "";
+     
+     System.out.println(text.isEmpty());       // false
+     System.out.println(emptyString.isEmpty()); // true
+     System.out.println("".equals(emptyString)); // true
+     
+     
+     String present=null;
+     System.out.println("".equals(present));  // false has it contains nulls
+     
+     String emptyspace="    ";
+     System.out.println(emptyspace.isBlank()); // true
+     
+     
+     
+	}
+}
